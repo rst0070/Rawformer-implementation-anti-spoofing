8aafc31fd27b33fe7c46a3d3ef854ee8c5f2e617
Wonbin Kim <kwb0711@gmail.com>
2023-07-11 16:39:12 +0900

    pre-emphasis

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class SysConfig:
         self.wandb_name                 = 'Rawformer-L, no DA'
         self.wandb_entity               = 'rst0070'
         self.wandb_key                  = '8c8d77ae7f92de2b007ad093af722aaae5f31003'
-        self.wandb_notes                = 'lr=8*1e-4, ts_hidden=80, rand_seed=1024, pre-emphasis'
+        self.wandb_notes                = 'lr=8*1e-4, ts_hidden=80, rand_seed=1024, pre-emphasis=0.97'
         
         self.path_label_asv_spoof_2019_la_train     = '/data/ASVspoof2019/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt'
         self.path_label_asv_spoof_2019_la_dev       = '/data/ASVspoof2019/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.dev.trl.txt'
@@ -25,6 +25,8 @@ class ExpConfig:
     def __init__(self):
         
         self.random_seed                = 1024
+        
+        self.pre_emphasis               = 0.97
         
         self.sample_rate                = 16000
         self.train_duration_sec         = 4
